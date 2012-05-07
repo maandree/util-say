83c431c37a0fd93269ab49580906f7e87bdde265
Mattias Andrée <maandree@operamail.com>
2012-05-07 22:33:44 +0200

    copy notice fix

--- src/se/kth/maandree/utilsay/Colour.java
+++ src/se/kth/maandree/utilsay/Colour.java
@@ -8,18 +8,18 @@
  * TWT — Terminal Window Toolkit, a free pure Java terminal toolkit.
  * Copyright (C) 2011 Mattias Andrée <maandree@kth.se>
  *
- * This program is free software: you can redistribute it and/or modify
+ * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 package se.kth.maandree.utilsay;
 
